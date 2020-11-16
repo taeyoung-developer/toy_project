66d803839f2271631b113bf9b7a26110f3c56a8d
taeyoung <learner1day@gmail.com>
2020-11-16 22:29:57 +0900

    fix file

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-
 from flask import Flask, render_template, jsonify, request, make_response
 import requests
+import json
+
 app = Flask(__name__)
 
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
 
 # HTML 화면 보여주기
@@ -10,24 +13,28 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 # @app.route('/main')
 # def main():
-#     return render_template('index.html')
-@app.route('/transfer',methods=['POST'])
+#     return render_template('index2.html')
+
+# 변경할 url 가져와 openapi로 보내고 json 형태로 data 받기
+@app.route('/transfer', methods=['POST'])
 def style_transfer():
+
+    # print(config['default']['secret-key'])
     content = request.form['content']
     style = request.form['style']
-    print(content, style)
+
     r = requests.post(
         "https://api.deepai.org/api/fast-style-transfer",
         files={
-            'content': open(content, 'rb'),
-            'style': open(style, 'rb'),
+            'content': content,
+            'style': style,
         },
-        headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
+        headers={'api-key': config['default']['secret-key']}
     )
     change_style = r.json()
-
 
     print(change_style)
     return change_style
--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,100 @@
 $(document).ready(function () {
 
 })
-$('#url').onkeypress(function (e) {
-    if (e.keyCode == 13) {
-        let url = $('#url').val();
-        let show = document.querySelector('input');
-        show.placeholder = "saved url :" + url;
-        let showurl = '<span>' + show.placeholder + '</span>'
-        let outurl = document.getElementById("selector");
-        outurl.innerHTML = showurl;
-        outurl.classList.add("active");
+
+function ShowUrlButton() {
+    let value = event.target.id;
+    let Hidenimg = $(".show_img").hide();
+    Hidenimg
+    let empty = $('#url').val('');
+    empty;
+    let Openbutton = $(".upload_button").show();
+    let Uploadurl = $(".upload_url").css("display", "flex");
+    let url = $("#url").css("display", "flex");
+    let Hidenlabel = $("#selector").hide();
+    if (value == "1") {
+        Openbutton;
+        Uploadurl;
+        url;
+        Hidenlabel;
+        let path = $(".photo_frames").attr("alt", "https://github.com/taeyoung-developer/toy_project/blob/master/static/img/la_muse.jpg?raw=true");
+    } else if (value == "2") {
+        Openbutton;
+        Uploadurl;
+        url;
+        Hidenlabel;
+        let path = $(".photo_frames").attr("alt", "https://github.com/taeyoung-developer/toy_project/blob/master/static/img/rain_princess.jpg?raw=true");
+
+    } else if (value == "3") {
+        Openbutton;
+        Uploadurl;
+        url;
+        Hidenlabel;
+        let path = $(".photo_frames").attr("alt", "https://github.com/taeyoung-developer/toy_project/blob/master/static/img/the_scream.jpg?raw=true");
+
+    } else if (value == "4") {
+        Openbutton;
+        Uploadurl;
+        url;
+        Hidenlabel;
+        let path = $(".photo_frames").attr("alt", "https://github.com/taeyoung-developer/toy_project/blob/master/static/img/the_shipwreck_of_the_minotaur.jpg?raw=true");
+
+    } else if (value == "5") {
+        Openbutton;
+        Uploadurl;
+        url;
+        Hidenlabel;
+        let path = $(".photo_frames").attr("alt", "https://github.com/taeyoung-developer/toy_project/blob/master/static/img/udnie.jpg?raw=true");
+
+    } else if (value == "6") {
+        Openbutton;
+        Uploadurl;
+        url;
+        Hidenlabel;
+        let path = $(".photo_frames").attr("alt", "https://github.com/taeyoung-developer/toy_project/blob/master/static/img/wave.jpg?raw=true");
+
+    } else {
+        $(".upload_url").css("display", "none")
     }
-    let inputurl = document.getElementById("url");
-    inputurl.addEventListener("change",);
-    $.ajax({
-        type: 'POST',
-        url: "/transfer",
-        data: {
-            content: content,
-            style: 'D:/privacy/transfer_style/static/img/wave.jpg'
-        },
-        dataType: 'text',
-        success: function (data) {
-            console.log(data.content)
-            console.log(data.style)
-        }
-    })
-})
+
 }
 
+
+function enter() {
+    if (event.keyCode == 13) {
+        let content = $('#url').val();
+        $('#url').css('display', 'none')
+        $('#selector').css({
+            'display': 'flex',
+            'background-color': 'rgb(106,123,139)',
+            'color': 'rgb(79, 84, 88)',
+            'font-weight': 'normal',
+            'text-transform': 'uppercase',
+            'text-align': 'center',
+            'letter-spacing': '2px',
+            'font-weight': 'bold'
+        });
+        let style = $(".photo_frames").attr("alt")
+        let show = '<span>saved url : </span>' + content;
+        let Outurl = document.getElementById("selector");
+        Outurl.innerHTML = show;
+        $.ajax({
+            type: 'POST',
+            url: "/transfer",
+            data: {
+                content: content,
+                style: style
+            },
+            dataType: 'json',
+            success: function (response) {
+                $(".upload_button").hide();
+                let img = response['output_url'];
+                $(".show_img").css("display", "flex");
+                $("#origin_img").attr("src", content);
+                $("#transfer_img").attr("src", img);
+                $("#transfer_url").attr("href", img);
+
+            }
+        })
+    }
+}
